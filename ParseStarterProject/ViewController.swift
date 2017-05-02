63a7474deb25e667ea5c73a70934ef6755da6da7
Varun Babu Pozhath <varunbabu008@gmail.com>
2017-05-02 11:07:03 +1000

    Intitial Commit. Set up AWS

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -16,32 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let testObject = PFObject(className: "TestObject2")
         
-        testObject["foo"] = "bar"
-        
-        testObject.saveInBackground { (success, error) -> Void in
-            
-            // added test for success 11th July 2016
-            
-            if success {
-                
-                print("Object has been saved.")
-                
-            } else {
-                
-                if error != nil {
-                    
-                    print (error)
-                    
-                } else {
-                    
-                    print ("Error")
-                }
-                
-            }
-            
-        }
         
     }
 
